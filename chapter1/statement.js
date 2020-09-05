1c282dfddf9ffd1d038b808fb64137e1a2301c7a
jake <guswns1853@gmail.com>
2020-09-05 17:12:23 +0900

    변수명을 명확하게 수정하라

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -54,27 +54,27 @@ function statement(invoice, plays) {
 }
 
 // statement 함수 내부에 있던 switch문을 별개의 함수로 추출 (함수 추출하기)
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function amountFor(aPerformance, play) {
+  let result = 0;
 
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 console.log(statement(invoices, plays));
