d972cc3263da150470b746545e0e7f36ca89db6c
jake <guswns1853@gmail.com>
2020-09-05 17:52:19 +0900

    volumeCredits의 값 누적 로직을 별도 for문으로 분리하라

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -33,11 +33,15 @@ function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
 
   for (let perf of invoice[0].performances) {
-    volumeCredits += volumeCreditsFor(perf);
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
+  }
+
+  // 값 누적 로직을 별도 for 문으로 분리 (반복문 쪼개기)
+  for (let perf of invoice[0].performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
