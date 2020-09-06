b8938bc04c05591a84cc0e5798a09afe222f319c
jake <guswns1853@gmail.com>
2020-09-06 15:31:25 +0900

    반복문을 파이프라인으로 바꿔라

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -78,19 +78,11 @@ function statement(invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.data;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
