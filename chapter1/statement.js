da0af6726956a0a3b306b2e5a6bdd1934cfa71b1
jake <guswns1853@gmail.com>
2020-09-06 15:52:24 +0900

    htmlStatement 함수를 작성하라

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -42,6 +42,24 @@ function renderPlainText(data, plays) {
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -51,3 +69,4 @@ function usd(aNumber) {
 }
 
 console.log(statement(invoices, plays));
+console.log(htmlStatement(invoices, plays));
