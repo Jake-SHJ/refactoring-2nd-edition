e6e89e3e8e74b75c031f52d0b51da8735ecd92b3
jake <guswns1853@gmail.com>
2020-09-06 16:44:34 +0900

    희극 공연의 공연료 계산을 해당 서브클래스로 옮기고, 슈퍼클래스의 amount()는 에러를 던지게 하라

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -5,22 +5,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case "tragedy":
-        throw "오류 발생";
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-    return result;
+    throw new Error("서브클래스에서 처리하도록 설계되었습니다.");
   }
 
   get volumeCredits() {
@@ -42,7 +27,16 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
 
 function createPerformanceCalculator(aPerformance, aPlay) {
   switch (aPlay.type) {
