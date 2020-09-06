54061deb0a018a022d63ca65a8b4da4eb4ad8b28
jake <guswns1853@gmail.com>
2020-09-06 16:37:47 +0900

    서브클래스를 준비하라

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -36,8 +36,19 @@ class PerformanceCalculator {
   }
 }
 
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 exports.createStatementData = function createStatementData(invoice, plays) {
