81278743815353eea493f4a22317ed84937e0096
jake <guswns1853@gmail.com>
2020-09-06 16:42:10 +0900

    비극 공연의 공연료 계산을 해당 서브클래스로 옮기고, 슈퍼클래스의 로직을 사용하지 않도록 오류를 던지게 하라

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -9,11 +9,7 @@ class PerformanceCalculator {
 
     switch (this.play.type) {
       case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw "오류 발생";
       case "comedy":
         result = 30000;
         if (this.performance.audience > 20) {
@@ -36,7 +32,15 @@ class PerformanceCalculator {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
 
 class ComedyCalculator extends PerformanceCalculator {}
 
