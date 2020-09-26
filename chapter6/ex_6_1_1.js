fb1281934ce2d161a4a203745d100a4a97e75da1
jake <guswns1853@gmail.com>
2020-09-26 10:58:20 +0900

    ex_6_1_1 중복 주석 제거

--- chapter6/ex_6_1_1.js
+++ chapter6/ex_6_1_1.js
@@ -30,12 +30,10 @@ function printOwing(invoice) {
 
   printBanner();
 
-  // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
-  // 마감일(dueDate)을 기록한다.
   const today = Clock.today;
   invoice.dueDate = new Date(
     today.getFullYear(),
@@ -43,7 +41,6 @@ function printOwing(invoice) {
     today.getDate() + 30
   );
 
-  // 세부 사항을 출력한다.
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
@@ -61,12 +58,10 @@ function printOwing(invoice) {
 
   printBanner();
 
-  // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
-  // 마감일(dueDate)을 기록한다.
   const today = Clock.today;
   invoice.dueDate = new Date(
     today.getFullYear(),
