c2234cfd1f051e2590b059884c7a526be7311b2f
jake <guswns1853@gmail.com>
2020-09-26 10:56:21 +0900

    ex_6_1_1.js 추가

---
+++ chapter6/ex_6_1_1.js
@@ -0,0 +1,90 @@
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  console.log("******************");
+  console.log("**** 고객 채무 ****");
+  console.log("******************");
+
+  // 미해결 채무(outstanding)를 계산한다.
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  // 마감일(dueDate)을 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  // 세부 사항을 출력한다.
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+/* 배너 출력 함수 추출 */
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  printBanner();
+
+  // 미해결 채무(outstanding)를 계산한다.
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  // 마감일(dueDate)을 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  // 세부 사항을 출력한다.
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function printBanner() {
+  console.log("******************");
+  console.log("**** 고객 채무 ****");
+  console.log("******************");
+}
+
+/* 세부 사항 출력 함수 추출 */
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  printBanner();
+
+  // 미해결 채무(outstanding)를 계산한다.
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  // 마감일(dueDate)을 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  printDetails();
+}
+
+function printBanner() {
+  console.log("******************");
+  console.log("**** 고객 채무 ****");
+  console.log("******************");
+}
+
+function printDetails() {
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
