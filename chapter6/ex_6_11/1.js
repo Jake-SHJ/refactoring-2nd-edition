fc4e361b29ba18e4d1fc62491591ce5ec25a9bfd
jake <guswns1853@gmail.com>
2020-10-11 10:25:44 +0900

    단계 쪼개기 예시 base code 작성

---
+++ chapter6/ex_6_11/1.js
@@ -0,0 +1,15 @@
+// 상품의 결제 금액을 계산하는 코드
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFree
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
