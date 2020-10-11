32db35c21cf050a71fec805f4ba07754dce23c8b
jake <guswns1853@gmail.com>
2020-10-11 10:38:40 +0900

    단계 쪼개기 예시 refactoring code 작성

--- chapter6/ex_6_11/1.js
+++ chapter6/ex_6_11/1.js
@@ -1,10 +1,12 @@
 // 상품의 결제 금액을 계산하는 코드
 function priceOrder(product, quantity, shippingMethod) {
+  // 상품 정보를 이용한 계산
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  // 배송 정보를 이용한 배송비 계산
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFree
@@ -12,4 +14,153 @@ function priceOrder(product, quantity, shippingMethod) {
   const shippingCost = quantity * shippingPerCase;
   const price = basePrice - discount + shippingCost;
   return price;
+}
+
+/**
+ * 배송비 계산 함수 추출
+ */
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFree
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
+
+/**
+ * 중간 데이터 구조 생성
+ */
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const priceData = {};
+  const price = applyShipping(
+    priceData,
+    basePrice,
+    shippingMethod,
+    quantity,
+    discount
+  );
+  return price;
+}
+
+function applyShipping(
+  priceData,
+  basePrice,
+  shippingMethod,
+  quantity,
+  discount
+) {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFree
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
+
+/**
+ * 데이터 구조 정리 (첫번째 단계에서 처리되는 모든 매개변수를 중간 데이터로 이동)
+ */
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const priceData = {
+    basePrice: basePrice,
+    quantity: quantity,
+    discount: discount,
+  };
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFree
+      : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
+  return price;
+}
+
+/**
+ * 첫 번째 단계 함수 추출
+ */
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  return {
+    basePrice: basePrice,
+    quantity: quantity,
+    discount: discount,
+  };
+}
+
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFree
+      : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
+  return price;
+}
+
+/**
+ * 불필요한 상수 제거
+ */
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  return {
+    basePrice: basePrice,
+    quantity: quantity,
+    discount: discount,
+  };
+}
+
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFree
+      : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  return priceData.basePrice - priceData.discount + shippingCost;
 }
